a69108b4ae3095e97f3d21daf967c2477f74cab5
moshyfawn <oleg.butorov.98@gmail.com>
2019-04-22 19:36:48 +0300

    theme toggle hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+import useTheme from "./hooks/useTheme";
+
+const App = () => {  
+    const { theme, toggleTheme } = useTheme();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+      className={`App ${theme}`}
+      style={{
+        background: theme === 'dark' ? '#000' : '#fff',
+        color: theme === 'dark' ? '#fff' : '#000',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <button type="button" onClick={toggleTheme}>
+					Switch theme
+				</button>
       </div>
     );
   }
-}
 
 export default App;
